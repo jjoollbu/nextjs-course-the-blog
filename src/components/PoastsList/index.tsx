3e4c03a7ff8282bf68e05c37b076ba3dcdaa65f4
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-10-29 17:59:20 -0300

    feat: queries and fixes in the spinloader

--- src/components/PoastsList/index.tsx
+++ src/components/PoastsList/index.tsx
@@ -1,13 +1,13 @@
-import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
 
 export async function PostsList() {
-  const post = await postRepository.findAll();
+  const posts = await findAllPublicPosts();
 
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-      {post.map((post) => {
+      {posts.slice(1).map((post) => {
         const postLink = `posts/${post.slug}`;
         return (
           <div className="flex flex-col gap-4 group" key={post.id}>
--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,17 +1,19 @@
 import { PostCoverImage } from "../PostCoverImage";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
 
-export function PostFeatured() {
-  const slug = "aa";
-  const postLink = `posts/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+  const postLink = `posts/${post.slug}`;
 
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
       <PostCoverImage
         linkProps={{ href: postLink }}
         imageProps={{
-          src: "/images/bryen_9.png",
-          alt: "Titulo do post",
+          src: post.coverImageUrl,
+          alt: post.title,
           width: 1200,
           height: 720,
           priority: true,
@@ -19,13 +21,11 @@ export function PostFeatured() {
       />
 
       <PostSummary
-        postHeading="h1"
         postLink={postLink}
-        createdAt={"2025-04-08T00:24:38.616Z"}
-        title={"Rotina matinal de pessoas altamente eficazes"}
-        excerpt={
-          "O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO."
-        }
+        postHeading="h1"
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
       />
     </section>
   );
