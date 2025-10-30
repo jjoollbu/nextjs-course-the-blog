ca6e7228b4b48887146e30a50a4046b49c71b00d
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-10-30 11:50:45 -0300

    feat: add dynamic routes

--- src/components/PoastsList/index.tsx
+++ src/components/PoastsList/index.tsx
@@ -8,7 +8,7 @@ export async function PostsList() {
   return (
     <div className="grid grid-cols-1 mb-16 gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {posts.slice(1).map((post) => {
-        const postLink = `posts/${post.slug}`;
+        const postLink = `post/${post.slug}`;
         return (
           <div className="flex flex-col gap-4 group" key={post.id}>
             <PostCoverImage
--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -5,7 +5,7 @@ import { findAllPublicPosts } from "@/lib/post/queries";
 export async function PostFeatured() {
   const posts = await findAllPublicPosts();
   const post = posts[0];
-  const postLink = `posts/${post.slug}`;
+  const postLink = `post/${post.slug}`;
 
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
