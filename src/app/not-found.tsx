2f38da35ef7aecb933046503d6450c7607a1788e
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-10-31 10:20:52 -0300

    feat: add Error message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,11 @@
-import clsx from "clsx";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className={clsx(
-          "min-h-80 bg-slate-900 text-slate-100 dark:bg-slate-100 dark:text-slate-900",
-          "mb-16 p-8 rounded-xl",
-          "flex items-center justify-center",
-          "text-center"
-        )}
-      >
-        <div>
-          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-          <p>
-            erro 404 - A página que você está procurando não existe neste site.
-          </p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle="Página não encontrada"
+      contentTitle="404"
+      content="erro 404 - A página que você está procurando não existe neste site."
+    />
   );
 }
---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import React from "react";
+
+type ErrorMessageProps = {
+  pageTitle: string;
+  contentTitle: string;
+  content: React.ReactNode;
+};
+
+export default function ErrorMessage({
+  pageTitle,
+  contentTitle,
+  content,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div
+        className={clsx(
+          "min-h-80 bg-slate-900 text-slate-100 dark:bg-slate-100 dark:text-slate-900",
+          "mb-16 p-8 rounded-xl",
+          "flex items-center justify-center",
+          "text-center"
+        )}
+      >
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">{contentTitle}</h1>
+          <div>{content}</div>
+        </div>
+      </div>
+    </>
+  );
+}
