b5801a60c7cad007405e2aae916addc8f30aba89
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-10-31 11:11:16 -0300

    feat: add root error page

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+'use client";';
+
+import ErrorMessage from "@/components/ErrorMessage";
+import { useEffect } from "react";
+
+type RootErrorPageProps = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function RootErrorPage({ error }: RootErrorPageProps) {
+  useEffect(() => {
+    // console.log(error);
+  }, [error]);
+
+  return (
+    <ErrorMessage
+      pageTitle="Internal Server Error"
+      contentTitle="501"
+      content="Ocorreu um erro interno no servidor. Por favor, tente novamente mais tarde."
+    />
+  );
+}
